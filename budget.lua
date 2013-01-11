868dc3049bf90029db2dc1392be8bc8c067f397a
onesmash <good122000@qq.com>
2013-01-11 23:22:39 +0800

    add budget

---
+++ budget.lua
@@ -0,0 +1,14 @@
+budget {
+	weddingLunch = 500 * 4,
+	weddingDinner = 4388 * 33,
+	weddingPhotos = 13000,
+	weddingShow = 35000,
+	weddingDress = 5000,
+	weddingGift = 5000,
+	cashGift = 2000,
+	Cars = 600 * 6,
+	cigarette = 1000 * 4,
+	wine = 100 * 40,
+	hotel = 4000,
+	parentsClothes = 5000
+}
